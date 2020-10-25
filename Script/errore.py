395ddf4f4dacc354734cce08e81972c6980ee480
persona1234 <50184213+persona1234@users.noreply.github.com>
2020-10-25 08:31:55 +0100

    Add files via upload

---
+++ Script/errore.py
@@ -0,0 +1,13 @@
+# Qui faremo degli errori. Si ho cambiato i colori dello sfondo e dei commenti.
+
+#print(testo)
+
+
+#risoluzione 1, stampare parola
+
+#print("testo")
+
+#risoluzione 2, variabile
+
+testo = 5
+print(testo)
