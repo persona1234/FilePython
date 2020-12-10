8ce33897ff7aabfb1d0ed61e74bdd86d1dfad7eb
persona1234 <50184213+persona1234@users.noreply.github.com>
2020-12-10 13:52:30 +0100

    Add files via upload

---
+++ Script/Funzioni/calcolo.py
@@ -0,0 +1,6 @@
+def calcolo(a,b): #definisco una funzione che prende come argomenti a e b
+    c = a+b
+    print(c)
+#print(c) #ERRORE
+calcolo(2,3) #richiamo la funzione con gli argomenti 2 e 3 
+
---
+++ Script/Funzioni/funzione_ciao.py
@@ -0,0 +1,6 @@
+
+def funzione_saluto(): #Definisco una funzione con il nome: "funzione_saluto"
+    print("Ciao a tutti!")
+    
+funzione_saluto()
+
