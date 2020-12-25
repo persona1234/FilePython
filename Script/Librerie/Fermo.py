78895ba70590d77c2ec9e0a7c17921bbb90fb215
persona1234 <50184213+persona1234@users.noreply.github.com>
2020-12-25 10:13:42 +0100

    Add files via upload

---
+++ Script/Librerie/Fermo.py
@@ -0,0 +1,7 @@
+import time
+
+print("Inizio")
+
+time.sleep(2) # valore espresso in secondi
+
+print("Fine")
---
+++ Script/Librerie/radice.py
@@ -0,0 +1,8 @@
+import math
+
+numero = int(input("Inserisci un numero: "))
+radice = math.sqrt(numero)
+p = math.pi
+print("Pi greco è ugale a:",p)
+print(f"La radice di {numero} è {radice}")
+
